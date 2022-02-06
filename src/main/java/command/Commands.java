658e53b7d9dbf01569bd49303d6cfdb4d689b340
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2022-02-06 07:27:27 +0200

    Added option to provide custom id

--- src/main/java/command/Commands.java
+++ src/main/java/command/Commands.java
@@ -1,21 +1,28 @@
 package command;
 
+import message.Registration;
+
 import java.util.UUID;
 import java.util.function.Consumer;
 
 public abstract class Commands {
-    public static  <C extends Command> void dispatch(Class<C> type, C payload) {
+    public static  <C extends Command> String dispatch(Class<C> type, C payload) {
+        return dispatch(type, payload, UUID.randomUUID().toString());
+    }
+
+    public static  <C extends Command> String dispatch(Class<C> type, C payload, String id) {
         CommandBus
                 .getInstance()
                 .getDispatcher()
                 .dispatch(new CommandMessage(
-                        UUID.randomUUID().toString(),
+                        id,
                         type,
                         payload));
+        return id;
     }
 
-    public static <C extends Command> void register(Class<C> type, Consumer<? extends CommandMessage> handler){
-        CommandBus
+    public static <C extends Command> Registration register(Class<C> type, Consumer<? extends CommandMessage> handler){
+        return CommandBus
                 .getInstance()
                 .getHandlerStore()
                 .register(type, new CommandHandler(handler));
