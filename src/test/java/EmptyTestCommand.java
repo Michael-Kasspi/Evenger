04e69fb7263ab2a4cf021ed5bda2b2e330096ff7
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2022-02-07 04:01:46 +0200

    change of directory

--- src/test/java/EmptyTestCommand.java
+++ src/test/java/EmptyTestCommand.java
@@ -1,12 +1,6 @@
-package commands;
-
 import command.Command;
 
 public class EmptyTestCommand implements Command {
-
-    public EmptyTestCommand() {
-        System.out.println("Created empty test command");
-    }
 
     @Override
     public String getId() {
