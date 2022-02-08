654bf1d3da0446d9e750e3686908630f02c9ab5c
Michael-Kasspi <68224982+Michael-Kasspi@users.noreply.github.com>
2022-02-08 14:11:42 +0200

    Commands temporarily uses concrete class SimpleCommandBus
    This class requires a redesign.

--- src/main/java/command/Commands.java
+++ src/main/java/command/Commands.java
@@ -5,13 +5,17 @@ import message.Registration;
 import java.util.UUID;
 import java.util.function.Consumer;
 
+/*
+ * TODO: Remove the use of concrete class SimpleCommandBus
+ * TODO: Define class specification
+ * */
 public abstract class Commands {
-    public static  <C extends Command> String dispatch(Class<C> type, C payload) {
+    public static <P extends Command> String dispatch(Class<P> type, P payload) {
         return dispatch(type, payload, UUID.randomUUID().toString());
     }
 
-    public static  <C extends Command> String dispatch(Class<C> type, C payload, String id) {
-        CommandBus
+    public static <P extends Command> String dispatch(Class<P> type, P payload, String id) {
+        SimpleCommandBus
                 .getInstance()
                 .getDispatcher()
                 .dispatch(new CommandMessage(
@@ -21,8 +25,8 @@ public abstract class Commands {
         return id;
     }
 
-    public static <C extends Command> Registration register(Class<C> type, Consumer<? extends CommandMessage> handler){
-        return CommandBus
+    public static <P extends Command> Registration register(Class<P> type, Consumer<? extends CommandMessage> handler) {
+        return SimpleCommandBus
                 .getInstance()
                 .getHandlerStore()
                 .register(type, new CommandHandler(handler));
